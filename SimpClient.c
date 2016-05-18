e798f0ef2070d09e572347d54a78bb2038dfbca1
TeagueLander <tlander121@hotmail.com>
2016-05-18 12:31:07 -0700

    Response working!

--- SimpClient.c
+++ SimpClient.c
@@ -15,7 +15,7 @@
 /* define maximal string and reply length, this is just an example.*/
 /* MAX_RES_LEN should be defined larger (e.g. 4096) in real testing. */
 #define MAX_STR_LEN 120
-#define MAX_RES_LEN 120
+#define MAX_RES_LEN 4096
 
 /* Constants */
 #define DEFAULT_PORT 80
@@ -74,8 +74,10 @@ parse_URI(char *uri, char *hostname, int *port, char *identifier)
 perform_http(int sockid, char *identifier)
 {
 	/* connect to server and retrieve response */
-	char recvBuff[MAX_RES_LEN]; memset(recvBuff, '0', sizeof(recvBuff));
-	char *query_get = "GET / HTTP/1.0\r\n\r\n";
+	char recvBuff[MAX_RES_LEN]; memset(recvBuff, '\0', sizeof(recvBuff));
+	char query_get[MAX_STR_LEN]; memset(query_get, '\0', sizeof(query_get));
+	snprintf(query_get, MAX_STR_LEN, "GET /%s HTTP/1.0\r\n\r\n", identifier);
+	printf("%s\n",query_get);
 	int query_len = strlen(query_get);
 
 	int total_sent_len = 0;
@@ -85,7 +87,7 @@ perform_http(int sockid, char *identifier)
 	printf("Wrote in, wait for response\n");
 	int o = read(sockid, &recvBuff, MAX_RES_LEN);
 
-	printf("%s\n",recvBuff);
+	printf("\n%s\n",recvBuff);
 
 	close(sockid);
 }
